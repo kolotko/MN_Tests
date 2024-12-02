3153d59dc0d0896c57bb3fd0fd20bb764e40c9a9
kolotko111 <milosznowicki96@wp.pl>
2024-12-02 16:42:55 +0100

    remove bin

---
+++ MN_Tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Customers.Api.Tests.Integration;
+
+public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
+{
+    private readonly HttpClient _httpClient;
+
+    public CustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+    {
+        _httpClient = appFactory.CreateClient();
+    }
+    
+    [Fact]
+    public async Task Get_ReturnsNotFount_WhenCustommerDoesNotExist()
+    {
+        var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+        
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
