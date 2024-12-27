e117b1ebe8ff77705734746bd1b1cf5e11a88104
kolotko111 <milosznowicki96@wp.pl>
2024-12-27 13:05:25 +0100

    Mutation Tests

---
+++ MN_AdvancedTests/MutationTests.ClassLibrary/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace MutationTests.ClassLibrary;
+
+public class Calculator
+{
+    public int Sum(int a, int b) => a + b;
+
+    public int Sub(int a, int b) => a - b;
+
+    public int Mult(int a, int b) => a * b;
+
+    public int Div(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException("Division by zero is not allowed.");
+        return a / b;
+    }
+}
---
+++ MN_AdvancedTests/MutationTests.Tests/CalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿using MutationTests.ClassLibrary;
+using Xunit;
+
+namespace MutationTests.Tests;
+
+public class CalculatorTests
+{
+    private readonly Calculator _calculator;
+
+    public CalculatorTests()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Theory]
+    [InlineData(5, 5, 10)]
+    public void Sum_ShouldReturnCorrectResult(int a, int b, int expected)
+    {
+        // Act
+        int result = _calculator.Sum(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 0)]
+    public void Sub_ShouldReturnCorrectResult(int a, int b, int expected)
+    {
+        // Act
+        int result = _calculator.Sub(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    public void Mult_ShouldReturnCorrectResult(int a, int b, int expected)
+    {
+        // Act
+        int result = _calculator.Mult(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    public void Div_ShouldReturnCorrectResult(int a, int b, int expected)
+    {
+        // Act
+        int result = _calculator.Div(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Div_ShouldThrowDivideByZeroException_WhenDividingByZero()
+    {
+        // Arrange
+        int a = 6;
+        int b = 0;
+
+        // Act & Assert
+        Assert.Throws<DivideByZeroException>(() => _calculator.Div(a, b));
+    }
+}
